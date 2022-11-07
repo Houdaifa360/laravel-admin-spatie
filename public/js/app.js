a19390a1cb46b36a4aea31d3eaf16bc92c711277
Houdaifa360 <boucena214@gmail.com>
2022-11-07 23:58:29 +0100

    End Commit

--- public/js/app.js
+++ public/js/app.js
@@ -5047,7 +5047,8 @@ __webpack_require__.r(__webpack_exports__);
 
 
 window.Alpine = alpinejs__WEBPACK_IMPORTED_MODULE_1__["default"];
-alpinejs__WEBPACK_IMPORTED_MODULE_1__["default"].start();
+
+// Alpine.start();
 
 /***/ }),
 
